d4db04dd40c6497843f60221e850d4e442cc29f2
erlippe98 <154066972+erlippe98@users.noreply.github.com>
2023-12-19 17:59:43 +0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,83 @@
+//GitHub API Library
+const { Octokit } = require("@octokit/rest");
+
+//Insert your GitHub tokens here in next format ['token1', 'token2', 'token'] 
+const gitTokens = ['']
+
+//Insert your GitHub names here in next format ['namel', 'name2', 'name'] 
+const gitNames = ['']
+
+function generateUniqueName () {
+return `result_${Math.floor(Math.random() * 150000000)}.txt`;
+}
+
+function generateRandomString(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomString = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        randomString += characters.charAt(randomIndex);
+    }
+
+    return randomString;
+}
+const result  = generateRandomString(12);
+
+const words = ["apple", "banana", "orange", "grape", "kiwi", "pear", "cat", "dog"];
+function getRandomWord() {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    return words[randomIndex];}
+const  randomWord  = getRandomWord();
+
+async function pushRandomName (repoOwner, token) {
+const octokit = new Octokit ({auth: token});
+
+try {
+
+const branchFilesData = await octokit.rest.repos.getContent({ 
+owner: repoOwner,
+repo: 'RSAEncryptorDecryptor',
+branch: 'main',
+});
+
+console.log(branchFilesData, 'branchFilesData')
+
+
+//Generate unique name for the file
+let randomName = generateUniqueName();
+
+//Check if file name is unique in the repo
+const isAlreadyFileNameExist = !! branchFilesData.data.find((user) => { 
+return user.path === randomName;
+});
+
+//If file name is not unique, generate new name
+if (isAlreadyFileNameExist) {
+pushRandomName (repoOwner, token);
+}
+
+// Push file to the repo
+await octokit.rest.repos.createOrUpdateFileContents({
+owner: repoOwner,
+repo: 'RSAEncryptorDecryptor',
+path: randomName,
+message: Buffer.from(randomWord).toString('base64'),
+content: Buffer.from(result).toString('base64'),
+branch: 'main',
+});
+console.log('Pushed fact to randomFacts');
+} catch (error) {
+console.error('Error pushing fact to randomFacts', error);
+}
+}
+
+//Function to start execution 
+function startExecution() {
+gitTokens.forEach((token, index) => {
+pushRandomName (gitNames[index], token);
+})
+}
+
+//Start execution
+startExecution();
